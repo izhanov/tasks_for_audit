b1ec05794f56d8d56c17785cc90c6af19ae5bca6
Izhanov <ibekable@gmail.com>
2019-07-28 22:12:17 +0600

    Added clicker

---
+++ clicker/core.js
@@ -0,0 +1,27 @@
+const randomBackgroundColor = (element) => {
+  const letters = "0123456789ABCDEF";
+  let color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[(Math.floor(Math.random() * 16))]
+  }
+  element.style.background = color
+};
+
+let clicks = 0;
+
+const clickCounter = (element, clicks) => {
+  element.innerHTML = clicks
+};
+
+document.addEventListener("DOMContentLoaded",(e) => {
+  const square = document.getElementById("square");
+  randomBackgroundColor(square)
+  clickCounter(square, clicks);
+  if (square) {
+    square.addEventListener('click', (e) => {
+      clicks++
+      randomBackgroundColor(square);
+      clickCounter(square, clicks);
+    });
+  }
+});
